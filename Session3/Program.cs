16cae595edc9a83d406d4524cafa0c6b97b43af5
Ziyad Amara <ziyadamara@gmail.com>
2026-08-25 18:46:13 +0300

    question1

--- Session3/Program.cs
+++ Session3/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
+            #region
 
+            // 1. Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+
+            bool isTheConversionOccur = false;
+
+            do
+            {
+                Console.Write("Enter the number: ");
+                isTheConversionOccur = int.TryParse(Console.ReadLine(), out int number);
+
+                if (isTheConversionOccur)
+                {
+                    if (number % 3 == 0 && number % 4 == 0)
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Cannot convert the input to a number.");
+                }
+
+            }
+            while (!isTheConversionOccur);
+
+            #endregion
         }
     }
 }
