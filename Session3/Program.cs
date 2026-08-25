9ab10866c5591464586799af34f029e5b34b44fc
Ziyad Amara <ziyadamara@gmail.com>
2026-08-25 19:01:43 +0300

    Question2

--- Session3/Program.cs
+++ Session3/Program.cs
@@ -8,6 +8,39 @@
 
             // 1. Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
 
+            //bool isTheConversionOccur = false;
+
+            //do
+            //{
+            //    Console.Write("Enter the number: ");
+            //    isTheConversionOccur = int.TryParse(Console.ReadLine(), out int number);
+
+            //    if (isTheConversionOccur)
+            //    {
+            //        if (number % 3 == 0 && number % 4 == 0)
+            //        {
+            //            Console.WriteLine("Yes");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("No");
+            //        }
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("Cannot convert the input to a number.");
+            //    }
+
+            //}
+            //while (!isTheConversionOccur);
+
+            #endregion
+
+            #region
+
+            // 2. Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
             bool isTheConversionOccur = false;
 
             do
@@ -17,13 +50,13 @@
 
                 if (isTheConversionOccur)
                 {
-                    if (number % 3 == 0 && number % 4 == 0)
+                    if (number < 0)
                     {
-                        Console.WriteLine("Yes");
+                        Console.WriteLine("Negative");
                     }
                     else
                     {
-                        Console.WriteLine("No");
+                        Console.WriteLine("Positive");
                     }
                 }
 
